90919b48aeda204a9ade82a91eab05d6a3a6a51b
xacer <chacecaven@outlook.com>
2023-06-29 16:10:13 -0400

    add relative paths

--- src/lib/sandbox.ts
+++ src/lib/sandbox.ts
@@ -2,7 +2,7 @@ import Sandbox, { type Options } from 'websandbox';
 
 export async function initSandbox(frameContainer: HTMLDivElement) {
     const localApi = {
-        makeWorkerScript: async() => await fetch("_worker.js").then(t=>t.blob()).then(t=>t.text())
+        makeWorkerScript: async() => await fetch("./_worker.js").then(t=>t.blob()).then(t=>t.text())
     };
 
     const sandboxOptions: Options = {
@@ -12,11 +12,11 @@ export async function initSandbox(frameContainer: HTMLDivElement) {
 
     const sandbox = await Sandbox.create(localApi, sandboxOptions).promise;
 
-    await sandbox.importScript("_sandbox.js");
+    await sandbox.importScript("./_sandbox.js");
 
     return {
         reload(code: string) {
-            sandbox.run("generateWorker(\"void \"+ (function main(){"+code+"}).toString() + \"();\");");
+            sandbox.run("generateWorker(\"void \"+ (function main(){"+code+"\n}).toString() + \"();\");");
         }
     };
 }
