d1649e2923b49d69b5f9b06b1d9676ed93a771b2
xacer <chacecaven@outlook.com>
2024-04-26 09:34:26 -0400

    hide more items of workerglobalscope

--- src/lib/sandbox.ts
+++ src/lib/sandbox.ts
@@ -16,7 +16,7 @@ export async function initSandbox(frameContainer: HTMLDivElement, width: number)
 
     function reload (code: string, regenerate: boolean = false) {
         // sandbox.run("generateWorker(\"void \"+ (function main(){ let canvas=null; let ctx=null; let raf=null; let event=null;"+code+"\n}).toString() + \"();\", " + regenerate.toString() + ");");
-        sandbox.run("generateWorker(\"class Program { start () {  let canvas=null; let ctx=null; let raf=null; let postMessage=null; let Program = null; \" + function () { { " + code + " } }.toString().substr(12) + \" } }; (new Program()).start.bind({})();\", " + regenerate.toString() + ");");
+        sandbox.run("generateWorker(\"class Program { start () {  let canvas=null; let ctx=null; let raf=null; let postMessage=null; let Program = null; let setTimeout = null; let setInterval = null; let clearTimeout = null; let clearInterval = null; let requestAnimationFrame = null; let clearAnimationFrame = null; \" + function () { { " + code + " } }.toString().substr(12) + \" } }; (new Program()).start.bind({})();\", " + regenerate.toString() + ");");
     }
 
     reload("");
